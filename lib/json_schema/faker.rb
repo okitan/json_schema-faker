bc4d070c55a397333055455e16a83a7c679d63f7
okitan <okitakunio@gmail.com>
2016-05-17 01:05:23 +0900

    add debug information (and should be logger in the next commit)

--- lib/json_schema/faker.rb
+++ lib/json_schema/faker.rb
@@ -11,34 +11,38 @@ module JsonSchema
     end
 
     def generate(hint: nil)
-      _generate(@schema, hint: nil)
+      _generate(@schema, hint: nil, position: "")
     end
 
     protected
-    def _generate(schema, hint: nil)
+    def _generate(schema, hint: nil, position:)
+      warn position
+
       # TODO: should support the combinations of them
       # http://json-schema.org/latest/json-schema-validation.html#anchor75
       if    !schema.one_of.empty?
       elsif !schema.any_of.empty?
       elsif !schema.all_of.empty?
       elsif !schema.properties.empty?
-        generate_properties(schema, hint: hint)
+        generate_properties(schema, hint: hint, position: position)
       elsif schema.enum
       elsif schema.type
-        generate_by_type(schema)
+        generate_by_type(schema, position: position)
       elsif schema.not
       else
         {} # consider as "type": "object"
       end
     end
 
-    def generate_properties(schema, hint: nil)
+    def generate_properties(schema, hint: nil, position:)
       schema.properties.each.with_object({}) do |(key, value), hash|
-        hash[key] = _generate(value, hint: nil) # TODO: pass hint
+        hash[key] = _generate(value, hint: nil, position: "#{position}/#{key}") # TODO: pass hint
       end
     end
 
-    def generate_by_type(schema, hint: nil)
+    def generate_by_type(schema, hint: nil, position:)
+      warn "generate by type for #{position}"
+
       # http://json-schema.org/latest/json-schema-core.html#anchor8
       case schema.type.first
       when "array"

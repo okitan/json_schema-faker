e533221d148515b6b60b32309b6ae8aaf53a8eb2
okitan <okitakunio@gmail.com>
2016-06-03 10:20:28 +0900

    allow strategy

--- spec/json_schema/suite_spec.rb
+++ spec/json_schema/suite_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe JsonSchema::Faker do
 
   Dir["suite/tests/draft4/**/*.json"].sort.each do |file|
     next if file == "suite/tests/draft4/definitions.json" # json shema does not support resolve ref over http
-    next if file == "suite/tests/draft4/ref.json"         # json shema does not support resolve ref over http
+    next if file == "suite/tests/draft4/ref.json"         # they should be root schema and when I do it, faker will always return {}
     next if file == "suite/tests/draft4/refRemote.json"   # json shema does not support resolve ref over http
 
     fcontext "from suite #{file}" do
